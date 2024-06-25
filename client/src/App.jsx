50bb2f196c4e33ad3dcf140fc91bcb6c9cb9dc81
Lokesh Ghosh <lghosh857@gmail.com>
2024-06-25 10:02:24 +0530

    client : setup and configuration

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-green-500 text-center font-bold">Grocery App</h1>
+    </>
+  );
+}
+
+export default App;
